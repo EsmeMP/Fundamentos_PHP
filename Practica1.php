13152b6ebcb69f81d92ebb074f2f9f7e4b80f026
EsmeMP <esmeralda.padilla.com@gmail.com>
2025-05-08 16:59:21 -0600

    09 Conexion con phpMyAdmin

--- Practica1.php
+++ Practica1.php
@@ -1,32 +1,20 @@
 <?php
-    // $json ='{"Galletas":"Gato", "Mailo":"Perro"}';
-    // var_dump(json_decode($json));
-
-    // //Sirve para obtener la ruta del archivo
-    // $conexion = file_get_contents("../PHP/datos.json");
-    // $mascotas = json_decode($conexion, true);
-
-    // //Imprimir los datos
-    // foreach($mascotas as $mascota){
-    //     print_r($mascota);
-    // }
-
-    // $json = '{"Name":"Galletas", "Type":"Gato"}';
-    
-    // $pets = json_decode($json, true);
-    // foreach($pets as $key => $value){
-    //     echo $key . "=>" . $value. "\n";
-    // }
-    
+include_once("conexion.php");
+$conexion = new Conexion();
+$conexion->ConexionBD();
 class Pets{    
         public $name;
         public $type;
+        public $age;
+        public $race;
 
 
-        function __construct($name, $type)
+        function __construct($name, $type, $age, $race )
         {
             $this->name = $name;
             $this->type = $type;
+            $this->age = $age;
+            $this->race = $race;
         }
         
         function get(){
@@ -47,35 +35,37 @@ class Pets{
            return $response;
         } 
 }   
+
+
         
-$pet1 = new Pets("Galletas", "Gato");
-echo $pet1->get_name();
-echo $pet1->get_type();
-$pet2 = new Pets("Mailo", "Perro");
+// $pet1 = new Pets("Galletas", "Gato");
+// echo $pet1->get_name();
+// echo $pet1->get_type();
+// $pet2 = new Pets("Mailo", "Perro");
 
-function petArray($pet){
-    return[
-        "name" => $pet->name,
-        "type" => $pet->type
-    ];
-}
-$conexion = "../PHP/datos.json";
-
-
-//Crear conexion de sql
-
-if(file_exists($conexion)){
-    $contenido = file_get_contents($conexion);
-    $arreglo = json_decode($contenido, true);
-}
-else{
-    $arreglo = [];
-}
-
-$arreglo[] = petArray($pet2);
-$jsonActualizado = json_encode($arreglo);
-file_put_contents($conexion, $jsonActualizado);
-print_r($jsonActualizado);
+// function petArray($pet){
+//     return[
+//         "name" => $pet->name,
+//         "type" => $pet->type
+//     ];
+// }
+// $conexion = "../PHP/datos.json";
 
 
-?>
+// //Crear conexion de sql
+
+// if(file_exists($conexion)){
+//     $contenido = file_get_contents($conexion);
+//     $arreglo = json_decode($contenido, true);
+// }
+// else{
+//     $arreglo = [];
+// }
+
+// $arreglo[] = petArray($pet2);
+// $jsonActualizado = json_encode($arreglo);
+// file_put_contents($conexion, $jsonActualizado);
+// print_r($jsonActualizado);
+
+
+// ?>
